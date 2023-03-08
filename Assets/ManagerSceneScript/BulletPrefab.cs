d1b4b86d595ffd1a4d4d8fb5e24379abe0b159c2
Goen Hee Han <gksrjsgml13@naver.com>
2023-03-08 09:05:32 +0900

    총알 반환 판별식 수정

--- Assets/ManagerSceneScript/BulletPrefab.cs
+++ Assets/ManagerSceneScript/BulletPrefab.cs
@@ -14,12 +14,10 @@ public class BulletPrefab : MonoBehaviour
 
     private Vector3[] endPoint;
     
-    private Ray _pivotRay;
-    private RaycastHit _raycastHit;
-    private int layerMask;
-    private Vector3 _rayOffset;
+    RaycastHit hit;
 
-    private GameObject ObjObstacle;
+    private int layerMask;
+
     private Action<GameObject> objectRecycle;
     private float distance;
     private float time;
@@ -28,33 +26,8 @@ public class BulletPrefab : MonoBehaviour
 
     private void Start()
     {
-        _rayOffset = new Vector3(0, 0, 5);
-        _pivotRay = new Ray();
-        _pivotRay.direction = Vector3.forward;
         layerMask = 1 << LayerMask.NameToLayer("Obstacle");
     }
-
-    private void GetObstacle()
-    {
-        _pivotRay.origin = transform.position + transform.rotation * _rayOffset;
-        if (Physics.Raycast(_pivotRay, out _raycastHit, 100f, layerMask))
-        {
-            Debug.Log("물체와의 거리 : " +  _raycastHit.distance);
-            ObjObstacle = _raycastHit.transform.gameObject;
-            distance = _raycastHit.distance;
-            StartCoroutine(MoveObject());
-        }
-        else
-        {
-            StartCoroutine(BulletMove());
-        }
-        ismove = true;
-    }
-
-    // private void OnEnable()
-    // {
-    //     GetObstacle();
-    // }
 
     private void OnDisable()
     {
@@ -63,45 +36,31 @@ public class BulletPrefab : MonoBehaviour
         objectRecycle = null;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        if(ismove)
-            transform.position += forward * Time.deltaTime * speed;
+        transform.position += forward * Time.deltaTime * speed;
+        
+        if (Physics.Raycast(transform.position, transform.forward, out hit, speed * Time.deltaTime, layerMask))
+        {
+            returnObjectPoolAction.Invoke(this);
+            objectRecycle.Invoke(hit.collider.gameObject);
+            gameObject.SetActive(false);
+        }
+        else if (!bounds.Contains(gameObject.transform.position))
+        {
+            returnObjectPoolAction.Invoke(this);
+            gameObject.SetActive(false);
+        }
     }
 
-    public void SetBullet(Vector3 startPosition, Quaternion rotation, Action<BulletPrefab> ReturnpoolAction, Bounds Bounds, Action<GameObject> ObstacleRecycle)
+    public void SetBullet(Vector3 StartPosition, Quaternion Rotation, Action<BulletPrefab> ReturnpoolAction, Bounds Bounds, Action<GameObject> ObstacleRecycle)
     {
-        this.transform.position = startPosition;
-        this.transform.rotation = rotation;
-        forward = rotation * Vector3.forward;
+        transform.position = StartPosition;
+        transform.rotation = Rotation;
+        forward = Rotation * Vector3.forward;
         returnObjectPoolAction = ReturnpoolAction;
         bounds = Bounds;
         objectRecycle = ObstacleRecycle;
         gameObject.SetActive(true);
-        GetObstacle();
-    }
-
-    IEnumerator BulletMove()
-    {
-        yield return new WaitUntil(() =>
-            bounds.Contains(this.gameObject.transform.position) == false
-        );
-        returnObjectPoolAction.Invoke(this);
-        gameObject.SetActive(false);
-    }
-
-    IEnumerator MoveObject()
-    {
-        time = distance / speed;
-
-        float elapsedTime = 0f;
-        while (elapsedTime < time)
-        {
-            elapsedTime += Time.fixedDeltaTime;
-            yield return null;
-        }
-        returnObjectPoolAction.Invoke(this);
-        objectRecycle.Invoke(ObjObstacle);
-        gameObject.SetActive(false);
     }
 }
